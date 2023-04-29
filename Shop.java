062b6a31b10c76b0e3983c1286275481a5a0006f
22Kamaj <112635517+22Kamaj@users.noreply.github.com>
2023-04-29 13:45:25 -0400

    Update Shop.java

--- Shop.java
+++ Shop.java
@@ -21,6 +21,7 @@ public class Shop {
 
     public String otherinput;
     public String output = "";
+    public double amount = 0;
 
     public String storeLayout() {
 
@@ -63,7 +64,6 @@ public class Shop {
     public double calcBoughtItem(String categoryRequest, int inputNumber, String town){
 
         double number = 0;
-        double amount = 0;
         buying = true;
         
        
@@ -74,7 +74,6 @@ public class Shop {
         else if (town.equalsIgnoreCase("Laramie")){
             
             amount = 1.25;
-         
         }
         else if (town.equalsIgnoreCase("Fort Bridger")){
             
@@ -212,57 +211,56 @@ public class Shop {
 
         if (userInput.equals("Food") || userInput.equals("1")) {
 
-            input = "You are buying food. Food is: .10 coffee, .02 flour, .05 bacon per pd. " +
-                    " How much do you want to buy: ";
-
+            input = "You are buying food. Food is: " + (.10 * amount) + " coffee," + (.02 * amount) + " flour," (.05* amount) + " bacon per pd in " 
+                + town + \n + " How much do you want to buy: ";
 
         } else if (userInput.equals("Clothing") || userInput.equals("2")) {
 
-            input = "You are buying clothes. Clothes are 20 cents. " +
+            input = "You are buying clothes. Clothes are" + (20 * amount) + " cents. " + \n +
                     "How much do you want to buy:";
 
         } else if (userInput.equals("Weapons") || userInput.equals("3")) {
 
-            input = "You are buying Weapons. Rifle: $20, Shotgun: $10, Shots: $5. " +
+            input = "You are buying Weapons. Rifle: $" + (20 * amount) + ", Shotgun: $" + (10 * amount) + ", Shots: $" + (5 * amount) + ". " + \n +
                     "What weapons do you want to buy and how many: ";
         }else if (userInput.equals("Oxen") || userInput.equals("4")) {
 
-            input = "You are buying Oxen. Oxen: $50. " +
+            input = "You are buying Oxen. Oxen: $" (50 * amount) + ". " + \n +
                     "How many do you want to buy: ";
 
         } else if (userInput.equals("Spare Wagon Wheel") || userInput.equals("5")) {
 
-            input = "You are buying a Spare Wagon Wheel. Spare wagon wheel: $8 each. " +
+            input = "You are buying a Spare Wagon Wheel. Spare wagon wheel: $" (8 * amount) " each. " + \n +
                     "How many do you want to buy: ";
 
         } else if (userInput.equals("Spare Wagon Axel") || userInput.equals("6")) {
 
-            input = "You are buying a Spare Wagon Axel. Spare wagon axel: $3 each. " +
+            input = "You are buying a Spare Wagon Axel. Spare wagon axel: $" (3 * amount) " each. " + \n +
                     "How any do you want to buy: ";
 
         } else if (userInput.equals("Spare Wagon Tongues") || userInput.equals("7")) {
 
-            input = "You are buying a Spare Wagon Tongues. Spare wagon tongues: $3 each. " +
+            input = "You are buying a Spare Wagon Tongues. Spare wagon tongues: $" + (3 * amount) + " each. " + \n +
                     "How any do you want to buy: ";
 
         } else if (userInput.equals("Medical Supply Box") || userInput.equals("8")) {
 
-            input = "You are buying a Medical Supply Box. Medical supply box: $1.50 each. " +
+            input = "You are buying a Medical Supply Box. Medical supply box: $" + (1.50* amount) + "each. " + \n +
                     "How any do you want to buy: ";
 
         } else if (userInput.equals("Sewing Kit") || userInput.equals("9")) {
 
-            input = "You are buying a Sewing Kit. Medical supply box: 0.50 cents each. " +
+            input = "You are buying a Sewing Kit. Medical supply box: " + (0.50 * amount) + " cents each. " + \n +
                     "How any do you want to buy: ";
 
         } else if (userInput.equals("Fire Starting Kit") || userInput.equals("10")) {
 
-            input = "You are buying a Fire Starting Kit. Fire starting kit: 0.25 cents each. " +
+            input = "You are buying a Fire Starting Kit. Fire starting kit: " + (0.25 * amount) + " cents each. " + \n +
                     "How any do you want to buy: ";
 
         } else if (userInput.equals("Kids Toys") || userInput.equals("11")) {
 
-            input = "You are buying Kids Toys. Kids toys: 0.05 cents each. " +
+            input = "You are buying Kids Toys. Kids toys: " + (0.05 * amount) + " cents each. " +
                     "How any do you want to buy: ";
 
         } else if (userInput.equals("Family Keepsakes") || userInput.equals("12")) {
@@ -271,17 +269,17 @@ public class Shop {
 
         } else if (userInput.equals("Seed packeges") || userInput.equals("13")) {
 
-            input = "You are buying Seed Packeges. Seed packeges: 0.01 cents per packet. " +
+            input = "You are buying Seed Packeges. Seed packeges: " + (0.01 * amount) + "cents per packet. " + \n +
                     "How any do you want to buy: ";
 
         } else if (userInput.equals("Shovels") || userInput.equals("14")) {
 
-            input = "You are buying Shovels. Shovels: $2.50 each. " +
+            input = "You are buying Shovels. Shovels: $" (2.50 * amount) + "each. " + \n
                     "How any do you want to buy: ";
 
         } else if (userInput.equals("Cooking items") || userInput.equals("15")) {
 
-            input = "You are buying Cooking items. Cooking items: $1 Coffee Mill, $1.50 frying pan, 0.25 cents pan. " +
+            input = "You are buying Cooking items. Cooking items: $" + (1 * amount) + " Coffee Mill, $" + (1.50 * amount) + " frying pan, " (0.25 * amount) + " cents pan. " + \n +
                     "How any do you want to buy: ";
 
         } else {
